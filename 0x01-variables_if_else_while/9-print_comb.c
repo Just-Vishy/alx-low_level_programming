932206db55075070b7b96a54bc4d08b412e833d5
Vishnu <veraciousvichy@gmail.com>
2022-12-12 11:24:04 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,18 @@ int main(void)
 {
 	int x;
 
-	for (x = '0' ; x <= '8' ; x++)
+	for (x = '0' ; x <= 9 ; x++)
 	{
 		putchar(x);
+
+	if (x != '9')
+	{
 		putchar(',');
 		putchar(' ');
 	}
+	}
 
-	putchar('9');
+	putchar('\n');
 
 	return (0);
 }
