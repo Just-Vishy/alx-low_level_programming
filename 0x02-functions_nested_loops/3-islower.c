0321f9f386e9f382f873a80373ec401edc3d1095
Vishnu <veraciousvichy@gmail.com>
2022-12-14 09:25:41 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,20 +2,23 @@
 #include <ctype.h>
 
 /**
- * _islower - Entry point
+ * _islower - lowercase value
  *
  * @c: is the character
  *
  * _putchar - prints out characters
+ *
+ * Return: 1 = lowercase else 0 is not
  */
 
 int _islower(int c)
 {
-	if (islower(c))
+	if (_islower(c))
 	{
-		return(1);
+		return (1);
 	}
-	else {
-		return(0);
+	else
+	{
+		return (0);
 	}
 }
