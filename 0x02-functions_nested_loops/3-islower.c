caeea14870bb261cc9b738679757c35e7c97505a
Vishnu <veraciousvichy@gmail.com>
2022-12-14 09:10:10 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Entry point
+ *
+ * @c: is the character
+ *
+ * _putchar - prints out characters
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return(1);
+	}
+	else {
+		return(0);
+	}
+}
