b6a6fb353bd78dd0c280da3c8c5466273fd9aa91
Vishnu <veraciousvichy@gmail.com>
2022-12-15 02:57:43 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void jack_bauer(void)
+{
+	int minutes;
+	int hours;
+
+	for (hours = 0 ; hours < 24 ; hours++)
+	{
+		for (minutes = 0 ; minutes < 59 ; minutes++)
+		{
+			_putchar(hours / 10 + 48);
+			_putchar(hours % 10 + 48);
+			_putchar(58);
+			_putchar(minutes / 10 + 48);
+			_putchar(minutes % 10 + 48);
+		}
+	}
+}
