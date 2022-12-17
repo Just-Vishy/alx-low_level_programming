3105d9685954b6cf3def98c95d50309be84137d0
Vishnu <veraciousvichy@gmail.com>
2022-12-16 23:47:28 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,20 +8,40 @@
 
 void times_table(void)
 {
-	int times;
-	int nums;
-	int combined;
+	int x;
+	int y;
+	int z;
 
-	while (times < 10)
+	x = 0;
+	while (x < 10)
 	{
-		nums = 0;
-		while (nums < 10)
+		y = 0;
+		while (y < 10)
 		{
-			combined = times * nums;
-
-			if (combined > 9)
+			z = x * y;
+			if (z > 9)
 			{
-				_putchar(combined / 10 + '0');
-				_putchar(combined % 10 + '0');
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
 			}
-			else if (nums != 0
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar(z + 48);
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
