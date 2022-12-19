f5a926fad74437f403316c82ec167b1ea801551d
Vishnu <veraciousvichy@gmail.com>
2022-12-19 02:02:21 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,34 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
+		{
+			while (n <= 98)
+			{
+				if (n != 98)
+				{
+					printf("%d, ", n);
+				}
+				else
+				{
+					printf("%d\n", n);
+				}
+				n++;
+			}
+		}
+	else if (n >= 98)
 	{
-		if (n != 98)
+		while (n >= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
+			n--;
 		}
-		else
-		{
-			printf("%d\n", n);
-		}
-		n++;
-	}
-
-	while (n > 99)
-	{
-		if (n != 98)
-		{
-			printf("%d, ", n);
-		}
-		else
-		{
-			printf("%d\n", n);
-		}
-		n--;
 	}
 }
